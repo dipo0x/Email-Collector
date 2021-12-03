befac9b8003c6748ee8ac0907ad7a1df0bd5af00
adesiyanoladipo <adesiyanoladipo1@gmail.com>
2021-12-03 08:26:56 +0100

    Added lead edit and post route

--- controllers/index.js
+++ controllers/index.js
@@ -24,8 +24,26 @@ exports.leads = function(req, res, next) {
 }
 
 exports.show_lead = function(req, res, next) {
-    const query = req.params
+    const query = req.params;
 	Lead.findOne(query).then(leads=>{
         res.render('lead', { title: 'Express', leads: leads });
+    })
+}
+
+exports.edit_lead = function(req, res, next) {
+    const query = req.params;
+	Lead.findOne(query).then(leads=>{
+        res.render('lead/edit_lead');
+    })
+}
+
+exports.post_edit_lead = function(req, res, next) {
+    const id = req.params.lead_id
+    const email = req.params.lead_email
+
+	const lead = Lead.findOne(query)
+    lead.email = email
+    lead.email.save().then(leads=>{
+        res.redirect("/leads/" + id, { title: 'Saved', leads: leads });
     })
 }
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-var landing = require('../controllers/index')
+var landing = require('../controllers/index');
 
 /* GET home page. */
 router.get('/', landing.get_landing);
 router.post('/', landing.post_lead);
 router.get('/leads', landing.leads);
 router.get('/leads/:lead_id', landing.show_lead);
+router.get('/leads/:lead_id/edit', landing.edit_lead);
+router.post('/leads/:lead_id/edit', landing.post_edit_lead);
 
 module.exports = router;
