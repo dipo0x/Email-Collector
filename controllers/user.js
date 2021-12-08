76ee1853a98be18fa932fa12b99c2056da1a4a05
adesiyanoladipo <adesiyanoladipo1@gmail.com>
2021-12-08 22:11:46 +0100

    Implemented error handling and form validation for signup route

--- controllers/user.js
+++ controllers/user.js
@@ -3,17 +3,19 @@ const passportAuth = require('../passport_setup');
 let bcrypt = require('bcrypt');
 const passport = require('passport');
 let flash = require('connect-flash');
-const { ValidateInput} = require('../utils/validator');
+const {validateRegisterInput, validateUser} = require('../utils/validator');
+
+////FUNCTIONS FOR THIS FILE ALONE/////
+let rerender_signup = function(errors, req, res, next) {
+    res.render('user/signup', {title: "Registration Page", emailErr: req.body["email"], errors: errors});
+}
+/////////////
 
 exports.login = function(req, res, next) {
     res.render('user/login', {formData: {}, errors: {}});
 }
 exports.signup = function(req, res, next) {
-    res.render('user/signup', {title: "Registration Page", formData: {}, errors: {}});
-}
-
-let rerender_signup = function(errors, req, res, next) {
-    res.render('user/signup', {title: "Registration Page", emailErr: req.body["email"], errors: errors});
+    res.render('user/signup', {title: "Registration Page", formData: {}, errors: {}, theErrors: {}});
 }
 
 exports.post_login = function(req, res, next) {
@@ -27,21 +29,22 @@ exports.post_login = function(req, res, next) {
 exports.post_signup = async function(req, res, next) {
     const theEmail = req.body.email
     const thePassword = req.body.password
-    const { errors } = ValidateInput(theEmail, thePassword);
-    if(errors){
-        rerender_signup(errors, req, res, next);
-        }
+    const { errors, valid } = validateRegisterInput(theEmail, thePassword);
+    const { theErrors, isValid } = validateUser(theEmail, next);
+    if(!valid  || !isValid){
+        rerender_signup(theErrors, errors, req, res, next);
+    }
     else{
-        let newUser = new userData({
-            email: theEmail,
-            password: await bcrypt.hash(thePassword, 10)
-        })
-        newUser.save().then(result=>{
-            passport.authenticate('local',{
-                successRedirect: '/',
-                failureRedirect: '/signup',
-                failureFlash: true
-            })(req, res, next);
+    let newUser = new userData({
+        email: theEmail,
+        password: await bcrypt.hash(thePassword, 10)
+    })
+    newUser.save().then(result=>{
+        passport.authenticate('local',{
+            successRedirect: '/',
+            failureRedirect: '/signup',
+            failureFlash: true
+        })(req, res, next);
         })
     }
 }
--- utils/validator.js
+++ utils/validator.js
@@ -1,28 +1,33 @@
 const userData = require('../models/user')
-const errors = {};
+const validator = require('validator');
 
-module.exports.ValidateInput = function(email, password, errors){
-		if(email.trim()=== ''){
-			errors.email = "Email cannot be empty";
-			}	
-		filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-		if (!filter.test(email)){
-			errors.email = "Not a valid email address";
-			}
-		if(password.trim() === ''){
-			errors.password = "Password cannot be empty";	
-			}
-		return { errors }
+module.exports.validateRegisterInput = (email, password) => {
+	const errors = {};
+	if (!validator.isEmail(email)){
+		errors["email"] = "Not a valid email address";
+		}
+	if(!validator.isAscii(password) === ''){
+		errors["password"] = "Not a valid password";	
+		}
+	if(!validator.isLength(password, {min:4, max: 12})){
+		errors["password"] = "Password ensure that your password has a minimum of 4 char and maximum of 12 char";	
+	}
+	return{
+		errors,
+		valid: Object.keys(errors).length < 1
+	}
 }
 
-// module.exports.ValidateInput = function(email, password){
-// 	new Promise(function(resolve, reject){
-// 		validateRegInput(email, password, errors);
-// 		userData.find({ email: email}).then(user=>{
-// 			if (user){
-// 				errors.email = "Email already in use"
-// 			}
-// 		})
-// 	})
-// 	return { errors }
-// }
+module.exports.validateUser = (email, next) => {
+	const theErrors = {};
+	userData.find({ email: email}).then(user=>{
+	if (user){
+		console.log("brooo")
+		theErrors["email"] = "Email already in use"
+		}	
+	})
+	return{
+		theErrors,
+		isValid: Object.keys(theErrors).length < 1
+	}
+}
