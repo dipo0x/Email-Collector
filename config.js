c3def76408cb038aebc7e1b7b34ff03d2864f804
Oladipo Adesiyan <adesiyanoladipo1@gmail.com>
2021-12-11 15:50:22 +0100

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-module.exports = ()=>{
-    mongoose.connect('mongodb+srv://<db_name>:<db_password>@cluster0.98ez5.mongodb.net/LeadAPP?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true },(err)=>{
-	if (!err){console.log('MongoDB connected')}
-	else {console.log('Error : ' + err)}
-    })}
