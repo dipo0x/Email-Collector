44e0033a878f7cf83f3d1072e920ddf8df4864f4
Oladipo Adesiyan <adesiyanoladipo1@gmail.com>
2021-12-11 15:49:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 module.exports = ()=>{
-    mongoose.connect('mongodb+srv://Ogeneral2006:Ogeneral2006@cluster0.98ez5.mongodb.net/LeadAPP?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true },(err)=>{
+    mongoose.connect('mongodb+srv://<db_name>:<db_password>@cluster0.98ez5.mongodb.net/LeadAPP?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true },(err)=>{
 	if (!err){console.log('MongoDB connected')}
 	else {console.log('Error : ' + err)}
     })}
